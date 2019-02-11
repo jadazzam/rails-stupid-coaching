013d2cb279b54454cc1e7e61d03d465bcba14f6c
Jad Azzam <jadazzam@me.com>
2019-02-11 14:26:55 +0100

    form coach (ask & answer) done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def answer
+    @message = params[:question]
+    if @message == "I'm going to work"
+      @response = 'Great'
+    elsif @message.end_with?("?")
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def ask
+  end
+end
